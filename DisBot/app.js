1455a8684b9a1bb6ddd5df8a8d73797171df543c
Perry Berman <superstargamesnetwork@gmail.com>
2016-01-05 21:17:13 -0700

    added bot
    added bot

---
+++ DisBot/app.js
@@ -0,0 +1,137 @@
+﻿//include discord
+
+var Discord = require("discord.js");
+//var opus = require('node-opus');
+var bot = new Discord.Client();
+var i = 0;
+var x = 0;
+var d = "";
+var fs = require('fs');
+
+var admins = "";
+
+var array = fs.readFileSync('file.txt').toString().split("\n");
+for (i in array) {
+    if (i == 0) {
+        admins = admins + "" + array[i];
+    } else {
+        admins = admins + ", " + array[i];
+    }
+}
+console.log("Users in admins: " + admins);
+
+// Get the email and password
+var AuthDetails = require("./auth.json");
+
+
+bot.login(AuthDetails.email, AuthDetails.password);
+
+//when the bot is ready
+bot.on("ready", function () {
+    console.log("Ready to begin! Serving in " + bot.channels.length + " channels");
+});
+
+//when the bot disconnects
+bot.on("disconnected", function () {
+    //alert the console
+    console.log("Disconnected!");
+    
+    //exit node.js with an error
+    process.exit(1);
+});
+
+//when the bot receives a message
+bot.on("message", function (msg) {
+    
+    if (msg.content.startsWith("https://discord.gg/") && msg.channel.isPrivate == true) {
+        bot.joinServer(msg.content);
+        console.log("joined a new server");
+    }
+    
+    if (!msg.content.startsWith("##")) return;
+    var usr = msg.author;
+    
+    msg.content = msg.content.substr(2);
+    
+    var dm = msg.channel.isPrivate;
+    
+    
+    if (msg.content.startsWith("admin")) {
+        if (array.indexOf(msg.author.username.toString()) != -1) {
+            x = array.legnth;
+            array[x] = msg.content.split(" ").slice(1).join(" ");
+            fs.appendFile("file.txt", "\n" + array[x]);
+            msg.reply(" successfully added: " + msg.content.split(" ").slice(1).join(" "));
+            console.log(msg.author.username.toString() + " successfully added: " + msg.content.split(" ").slice(1).join(" "));
+        } else {
+            console.log(msg.author.username.toString() + " unsuccessfully tried to add: " + msg.content.split(" ").slice(1).join(" "));
+            msg.reply(" warning you tried to add: " + msg.content.split(" ").slice(1).join(" ") + " but don't have perms! do it again and an admin will be notified");
+        }
+    }
+    
+    if (msg.content == "help") {
+        bot.sendMessage(msg.channel, [
+            "Available commands are",
+            "```##ping",
+            "##help",
+            "##slap```",
+        ].join("\n"));
+        console.log("The user " + msg.author.username.toString() + " used the help command");
+    }
+    
+    if (msg.content == "ping") {
+        msg.reply(["PONG!"]);
+        console.log("The user " + msg.author.username.toString() + " used the pong command");
+    }
+    
+    if (msg.content.startsWith("play")) {
+        if (array.indexOf(msg.author.username.toString()) != -1 && dm) {
+            var game = msg.content.split(" ").slice(1).join(" ");
+            bot.setPlayingGame(game);
+            console.log("The user " + msg.author.username.toString() + " used the play command");
+        } else {
+            msg.reply("You do not have permission to use this command and/or this is not private messages");
+            console.log("The user " + msg.author.username.toString() + " tried to use the play command");
+        }
+
+		
+    }
+    
+    if (msg.content.startsWith("ttssay") && array.indexOf(msg.author.username.toString()) != -1) {
+        bot.sendTTSMessage(msg.channel, msg.content.split(" ").slice(1).join(" "));
+        console.log("The user " + msg.author.username.toString() + " tried to use the say command");
+    }
+    
+    if (msg.content == "test") {
+        if (!msg.channel.isPrivate) {
+            bot.sendMessage(msg.channel, msg.channel.server.rolesOf(msg.author));
+            console.log(msg.channel.server.rolesOf(usr));
+        } else {
+            bot.sendMessage(msg.channel, "This command can not be used in private chat");
+        }
+    }
+    
+    if (msg.content.startsWith("say")) {
+        bot.sendMessage(msg.channel, msg.content.split(" ").slice(1).join(" "));
+        console.log("The user " + msg.author.username.toString() + " tried to use the say command");
+    }
+    
+    if (msg.content == "stats") {
+        msg.reply([
+            "I am connected/have access to:",
+            bot.servers.length + " servers",
+            bot.channels.length + " channels",
+            bot.users.length + " users",
+        ].join("\n"));
+        console.log("The user " + msg.author.username.toString() + " tried to use the stats command");
+    }
+    
+    if (msg.content.startsWith("slap")) {
+        var randNumMin = 0;
+        var randNumMax = 8;
+        var pick = (Math.floor(Math.random() * (randNumMax - randNumMin + 1)) + randNumMin);
+        var fish = ["clown fish", "sword fish", "shark", "sponge cake", "blue berry pie", "banana", "poptart", "the blunt of a sword", "websters dicitonary: hardcopy"];
+        bot.sendMessage(msg.channel, msg.content.split(" ").slice(1).join(" ") + " got slapped with a " + fish[pick] + " by " + msg.author.toString());
+    }
+
+});
