0735ce079d47840e71ec89b067ce3aa397e1f8d2
Zachary <r3alcl0ud@gmail.com>
2017-03-10 10:27:54 -0700

    Hello

--- r3alb0t/src/main/java/xyz/r3alb0t/r3alb0t/Main.java
+++ r3alb0t/src/main/java/xyz/r3alb0t/r3alb0t/Main.java
@@ -1,6 +1,6 @@
 package xyz.r3alb0t.r3alb0t;
 
-import io.discloader.discloader.client.command.CommandHandler;
+import io.discloader.discloader.common.DLOptions;
 import io.discloader.discloader.common.DiscLoader;
 import xyz.r3alb0t.r3alb0t.common.EventHandler;
 
@@ -10,9 +10,11 @@ import xyz.r3alb0t.r3alb0t.common.EventHandler;
  */
 public class Main {
 	public static void main(String[] args) {
-		DiscLoader loader = new DiscLoader();
-		CommandHandler.prefix = "#$";
+		DLOptions options = new DLOptions("MTc0ODk4NzIyNDkxMjAzNTg0.C6G8wQ.4TabctIUZG4n0FMoRYdrJTtDp2o", "#$", true,
+				true);
+		DiscLoader loader = new DiscLoader(options);
 		DiscLoader.addEventHandler(new EventHandler());
-		loader.login("MTc0ODk4NzIyNDkxMjAzNTg0.C6G8wQ.4TabctIUZG4n0FMoRYdrJTtDp2o");
+//		DiscLoader.addEventHandler(new GUIEvents());
+		loader.login();
 	}
 }
