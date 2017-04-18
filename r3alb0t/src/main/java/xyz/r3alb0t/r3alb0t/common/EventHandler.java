5dc47a002785cb14fb3d4619231b418b1c6d90b5
Zachary <r3alcl0ud@gmail.com>
2017-04-18 12:29:41 -0700

    Fixed some voice stuff

--- r3alb0t/src/main/java/xyz/r3alb0t/r3alb0t/common/EventHandler.java
+++ r3alb0t/src/main/java/xyz/r3alb0t/r3alb0t/common/EventHandler.java
@@ -1,21 +1,32 @@
 package xyz.r3alb0t.r3alb0t.common;
 
+import com.neovisionaries.ws.client.WebSocketFrame;
+
 import io.discloader.discloader.common.event.DLPreInitEvent;
 import io.discloader.discloader.common.event.EventListenerAdapter;
+import io.discloader.discloader.common.event.RawEvent;
 import io.discloader.discloader.common.event.ReadyEvent;
 import xyz.r3alb0t.r3alb0t.music.RBMusic;
 
 public class EventHandler extends EventListenerAdapter {
-	
+
+	@Override
+	public void RawPacket(RawEvent data) {
+		if (data.isGateway()) {
+			WebSocketFrame frame = data.getFrame();
+			if (frame.isTextFrame()) System.out.println(frame.getPayloadText());
+		}
+	}
+
 	@Override
 	public void PreInit(DLPreInitEvent e) {
 		System.out.println("Registering music commands");
 		RBMusic.registerCommands();
 	}
-	
+
 	@Override
 	public void Ready(ReadyEvent event) {
 		event.loader.user.setGame("Converting to DiscLoader");
 	}
-	
+
 }
--- r3alb0t/src/main/java/xyz/r3alb0t/r3alb0t/music/CommandPlay.java
+++ r3alb0t/src/main/java/xyz/r3alb0t/r3alb0t/music/CommandPlay.java
@@ -5,28 +5,30 @@ import java.util.regex.Pattern;
 
 import io.discloader.discloader.client.command.Command;
 import io.discloader.discloader.common.event.message.MessageCreateEvent;
+import io.discloader.discloader.common.registry.EntityRegistry;
 import io.discloader.discloader.entity.message.IMessage;
 
 public class CommandPlay extends Command {
-    public CommandPlay() {
-        setUnlocalizedName("play");
-        setDescription("adds a track/set to the playlist\nCurrently only works with YouTube");
-    }
 
-    private final String regex = "\\#\\$play[ ](.*)";
-    private final Pattern pattern = Pattern.compile(regex);
+	public CommandPlay() {
+		setUnlocalizedName("play");
+		setDescription("adds a track/set to the playlist\nCurrently only works with YouTube");
+	}
 
-    @Override
+	private final String regex = "\\#\\$play[ ](.*)";
+	private final Pattern pattern = Pattern.compile(regex);
+
+	@Override
 	public void execute(MessageCreateEvent e, String[] args) {
 		IMessage message = e.getMessage();
-		if (message.getGuild() != null && e.loader.voiceConnections.containsKey(message.getGuild().getID())) {
+		if (message.getGuild() != null && EntityRegistry.hasVoiceConnection(message.getGuild().getID())) {
 			Matcher track = pattern.matcher(message.getContent());
-            if (track.find()) {
-                String trackID = track.group(1);
-                System.out.println(trackID);
-				e.loader.voiceConnections.get(message.getGuild().getID()).play(trackID);
-            }
-        } else {
-        }
-    }
+			if (track.find()) {
+				String trackID = track.group(1);
+				System.out.println(trackID);
+				EntityRegistry.getVoiceConnectionByGuild(message.getGuild()).play(trackID);
+			}
+		} else {
+		}
+	}
 }
