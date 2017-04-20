33a0db2f95ddded932c3f743c298beb5d333f0fd
Zachary <r3alcl0ud@gmail.com>
2017-04-20 15:49:26 -0700

    more logs stuff

--- r3alb0t/src/main/java/xyz/r3alb0t/r3alb0t/logs/LogHandler.java
+++ r3alb0t/src/main/java/xyz/r3alb0t/r3alb0t/logs/LogHandler.java
@@ -1,30 +1,40 @@
 package xyz.r3alb0t.r3alb0t.logs;
 
+import java.io.IOException;
 import java.time.OffsetDateTime;
 
+import io.discloader.discloader.client.render.util.Resource;
 import io.discloader.discloader.common.event.EventListenerAdapter;
 import io.discloader.discloader.common.event.guild.member.GuildMemberEvent.VoiceJoinEvent;
 import io.discloader.discloader.common.event.guild.member.GuildMemberEvent.VoiceLeaveEvent;
 import io.discloader.discloader.common.event.guild.member.GuildMemberEvent.VoiceSwitchEvent;
 import io.discloader.discloader.common.event.guild.member.GuildMemberRemoveEvent;
 import io.discloader.discloader.common.event.guild.member.GuildMemberUpdateEvent;
-import io.discloader.discloader.common.registry.EntityRegistry;
+import io.discloader.discloader.common.event.message.MessageDeleteEvent;
 import io.discloader.discloader.core.entity.RichEmbed;
 import io.discloader.discloader.entity.channel.IGuildVoiceChannel;
 import io.discloader.discloader.entity.channel.ITextChannel;
 import io.discloader.discloader.entity.guild.IGuild;
 import io.discloader.discloader.entity.guild.IGuildMember;
-import io.discloader.discloader.util.DLUtil.Endpoints;
+import io.discloader.discloader.entity.message.IMessage;
 
 public class LogHandler extends EventListenerAdapter {
 
+	private Resource vswitch = new Resource("r3alb0t", "texture/icon/logs/voiceSwitch.png");
+	private Resource vjoin = new Resource("r3alb0t", "texture/icon/logs/voiceJoin.png");
+	private Resource vleave = new Resource("r3alb0t", "texture/icon/logs/voiceLeave.png");
+
 	@Override
 	public void GuildMemberRemove(GuildMemberRemoveEvent event) {
-		IGuildMember member = event.getMember();
-		ITextChannel testChannel = EntityRegistry.getTextChannelByID(282230026869669888L);
-		RichEmbed embed = new RichEmbed(member.toString()).setDescription("Left the guild").setColor(0xf10000).setThumbnail(Endpoints.avatar(member.getID(), member.getUser().getAvatar().toString()));
-		embed.setTimestamp(OffsetDateTime.now());
-		testChannel.sendEmbed(embed);
+		// IGuildMember member = event.getMember();
+		// ITextChannel testChannel =
+		// EntityRegistry.getTextChannelByID(282230026869669888L);
+		// RichEmbed embed = new
+		// RichEmbed(member.toString()).setDescription("Left the
+		// guild").setColor(0xf10000).setThumbnail(Endpoints.avatar(member.getID(),
+		// member.getUser().getAvatar().toString()));
+		// embed.setTimestamp(OffsetDateTime.now());
+		// testChannel.sendEmbed(embed);
 	}
 
 	@Override
@@ -61,6 +71,11 @@ public class LogHandler extends EventListenerAdapter {
 		RichEmbed embed = new RichEmbed("Joined a Voice Channel").setTimestamp(OffsetDateTime.now()).setColor(0x00fa00);
 		embed.addField("Member", String.format("**%s** (ID: %d)", member.getNickname(), member.getID()));
 		embed.addField("Voice Channel", String.format("**%s** (ID: %d)", voiceChannel.getName(), voiceChannel.getID()), true);
+		try {
+			embed.setThumbnail(vjoin.getFile());
+		} catch (IOException e) {
+			// e.printStackTrace();
+		}
 		channel.sendEmbed(embed);
 	}
 
@@ -74,9 +89,14 @@ public class LogHandler extends EventListenerAdapter {
 		RichEmbed embed = new RichEmbed("Left Voice Channel").setTimestamp(OffsetDateTime.now()).setColor(0xa93d3d);
 		embed.addField("Member", String.format("**%s** (ID: %d)", member.getNickname(), member.getID()));
 		embed.addField("Voice Channel", String.format("**%s** (ID: %d)", voiceChannel.getName(), voiceChannel.getID()), true);
+		try {
+			embed.setThumbnail(vleave.getFile());
+		} catch (IOException e) {
+			// e.printStackTrace();
+		}
 		channel.sendEmbed(embed);
 	}
-	
+
 	@Override
 	public void GuildMemberVoiceSwitch(VoiceSwitchEvent event) {
 		if (event.getGuild().getID() != 282226852616077312l) return;
@@ -88,6 +108,29 @@ public class LogHandler extends EventListenerAdapter {
 		embed.addField("Member", String.format("**%s** (ID: %d)", member.getNickname(), member.getID()));
 		embed.addField("Previous Voice Channel", String.format("**%s** (ID: %d)", oldVoiceChannel.getName(), oldVoiceChannel.getID()), true);
 		embed.addField("Current Voice Channel", String.format("**%s** (ID: %d)", voiceChannel.getName(), voiceChannel.getID()), true);
+		try {
+			embed.setThumbnail(vswitch.getFile());
+		} catch (IOException e) {
+		}
+		channel.sendEmbed(embed);
+	}
+
+	// @Override
+	// public void MessageDelete(MessageDeleteEvent event) {
+	// System.out.println("test");
+	// }
+
+	@Override
+	public void MessageDelete(MessageDeleteEvent event) {
+		IMessage message = event.getMessage();
+		IGuild guild = message.getGuild();
+		if (guild == null || guild.getID() != 282226852616077312l) return;
+		ITextChannel channel = guild.getTextChannelByName("serverlog");
+		RichEmbed embed = new RichEmbed("Message Deleted").setTimestamp(OffsetDateTime.now());
+		System.out.println("" + message.getContent());
+		embed.addField("Channel", event.getChannel().toMention(), true);
+		embed.addField("Author", String.format("%s (ID: %d)", message.getAuthor().asMention(), message.getAuthor().getID()), true);
+		embed.addField("Message Contents", message.getContent(), true);
 		channel.sendEmbed(embed);
 	}
 }
