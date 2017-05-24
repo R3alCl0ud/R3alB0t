328d5d005a2b4b68c1b65fc56b6f403396ed2a73
Zachary <r3alcl0ud@gmail.com>
2017-05-24 08:35:28 -0700

    created an options file

--- r3alb0t/src/main/java/xyz/r3alb0t/r3alb0t/R3alB0t.java
+++ r3alb0t/src/main/java/xyz/r3alb0t/r3alb0t/R3alB0t.java
@@ -1,18 +1,53 @@
 package xyz.r3alb0t.r3alb0t;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.logging.Logger;
+
+import com.google.gson.Gson;
+
+import io.discloader.discloader.client.logger.DLLogger;
 import io.discloader.discloader.common.DLOptions;
 import io.discloader.discloader.common.DiscLoader;
 import xyz.r3alb0t.r3alb0t.common.EventHandler;
+import xyz.r3alb0t.r3alb0t.config.Config;
 
 /**
  * Hello world!
- *
  */
-public class Main {
-    public static void main(String[] args) {
-        DLOptions options = new DLOptions("MTc0ODk4NzIyNDkxMjAzNTg0.C6G8wQ.4TabctIUZG4n0FMoRYdrJTtDp2o", "#$");
-        DiscLoader loader = new DiscLoader(options);
-        loader.addEventHandler(new EventHandler());
-        loader.login();
-    }
+public class R3alB0t {
+
+	public static final Logger logger = new DLLogger(R3alB0t.class).getLogger();
+
+	public static Config config;
+	public static Gson gson = new Gson();
+
+	public static void main(String[] args) {
+		try {
+			String content = "";
+			File options = new File("options.json");
+			if (options.exists() && !options.isDirectory()) {
+				Object[] lines = Files.readAllLines(Paths.get("./options.json")).toArray();
+				for (Object line : lines)
+					content += line;
+				config = gson.fromJson(content, Config.class);
+			} else if (!options.exists() || options.isDirectory()) {
+				config = new Config();
+				
+				FileWriter fw = new FileWriter(options);
+				fw.write(gson.toJson(config));
+				fw.close();
+			}
+			System.out.println(gson.toJson(config));
+			
+			DLOptions dlOptions = new DLOptions(config.auth.token, config.prefix, true, false);
+			DiscLoader loader = new DiscLoader(dlOptions);
+			loader.addEventHandler(new EventHandler());
+			loader.login();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
---
+++ r3alb0t/src/main/java/xyz/r3alb0t/r3alb0t/config/Config.java
@@ -0,0 +1,14 @@
+package xyz.r3alb0t.r3alb0t.config;
+
+public class Config {
+
+	public String prefix = "c!";
+	public Auth auth = new Auth();
+	public class Auth {
+
+		public String token = "TOKEN";
+		public String dbIP = "localhost";
+		public String dbPassword = "PASSWORD";
+	}
+
+}
