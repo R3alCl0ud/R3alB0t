6177dcb0d1f18efa73292c197d54dbad730fb7a3
Perry Berman <perryberman.30@gmail.com>
2018-04-19 12:59:40 -0700

    Update Command info

--- r3alb0t/src/main/java/xyz/r3alb0t/r3alb0t/common/commands/CommandInfo.java
+++ r3alb0t/src/main/java/xyz/r3alb0t/r3alb0t/common/commands/CommandInfo.java
@@ -17,13 +17,14 @@ public class CommandInfo extends Command {
 	}
 
 	public void execute(MessageCreateEvent event, String[] args) {
-		RichEmbed embed = new RichEmbed("Client Info").setColor(0x1010dd);
-		embed.setDescription("Displaying info about the bot's client");
+		RichEmbed embed = new RichEmbed("General Information").setColor(0x42f46b);
+		embed.setDescription("Displaying information about R3alB0t");
 		embed.setThumbnail(event.getLoader().getSelfUser().getAvatar().toString());
 		embed.addField("Username", event.getLoader().getSelfUser().toString(), true);
 		embed.addField("Servers", "Connected to " + EntityRegistry.getGuildsOnShard(event.getLoader().getShard()).size() + " of " + EntityRegistry.getGuilds().size() + " server(s)", true);
 		embed.addField("Users", "Serving a total of " + EntityRegistry.getUsers().size() + " user(s)", true);
 		embed.addField("Shard Number", "" + (event.getLoader().shardid + 1) + " of " + event.getLoader().shards, true);
+		embed.addField("Website", "https://r3alb0t.xyz", true).addField("Discord Server", "https://discord.gg/012fGm4RhRlUxlLG5", true);
 		embed.setTimestamp();
 		embed.setFooter(R3alB0t.getCopyrightInfo());
 		event.getChannel().sendEmbed(embed);
