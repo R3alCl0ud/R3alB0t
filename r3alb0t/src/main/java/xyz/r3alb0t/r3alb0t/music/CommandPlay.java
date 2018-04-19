a03779d7281da316971015c64ba11dc2b137f405
Perry Berman <perryberman.30@gmail.com>
2018-04-19 13:21:49 -0700

    Fixed the Play command

--- r3alb0t/src/main/java/xyz/r3alb0t/r3alb0t/music/CommandPlay.java
+++ r3alb0t/src/main/java/xyz/r3alb0t/r3alb0t/music/CommandPlay.java
@@ -41,7 +41,60 @@ public class CommandPlay extends Command {
 		IGuildMember member = message.getMember();
 		ITextChannel channel = message.getChannel();
 		if (guild != null && RBMusic.plManagers.containsKey(guild.getID())) {
+			PlaylistManager plManager = RBMusic.plManagers.get(guild.getID());
+			if (args.length > 0) {
+				String trackID = args[0];
+				if (trackID.length() < 1) {
+					return;
+				}
+				if (!trackID.toLowerCase().startsWith("http"))
+					trackID = "ytsearch:" + trackID;
+				plManager.getVoiceConnection().findTrackOrTracks(trackID, new AudioLoadResultHandler() {
 
+					@Override
+					public void trackLoaded(AudioTrack track) {
+						plManager.trackLoaded(track, message.getAuthor());
+					}
+
+					@Override
+					public void playlistLoaded(AudioPlaylist playlist) {
+						if (!playlist.isSearchResult()) {
+							plManager.playlistLoaded(playlist);
+							return;
+						}
+						RBMusic.searchResults.put(guild.getID(), playlist);
+						RichEmbed embed = new RichEmbed("Search Results").setColor(0x28a7f6).setDescription("Showing search results for: " + args[0]);
+						for (int i = 0; i < 10; i++) {
+							AudioTrackInfo info = playlist.getTracks().get(i).getInfo();
+							String name = String.format("%d - %s", i + 1, playlist.getName());
+							String t = String.format("[*%s*](%s)  by %s", info.title, info.uri, info.author);
+							embed.addField(name, t);
+						}
+						embed.addField("Select a track", String.format("Type `%sselect <number>` to select a track from the search results", CommandHandler.prefix));
+						embed.setFooter(R3alB0t.getCopyrightInfo()).setTimestamp(OffsetDateTime.now());
+						channel.sendEmbed(embed);
+					}
+
+					@Override
+					public void noMatches() {
+						channel.sendMessage(String.format("No audio found at: %s", args[0]));
+					}
+
+					@Override
+					public void loadFailed(FriendlyException exception) {
+						exception.printStackTrace();
+						String err = exception.toString();
+						StackTraceElement[] trace = exception.getStackTrace();
+						for (StackTraceElement traceElement : trace) {
+							err += ("\tat " + traceElement);
+						}
+						channel.sendMessage(err);
+					}
+
+				});
+			} else {
+				plManager.startNext();
+			}
 		} else if (guild != null && member != null && !RBMusic.plManagers.containsKey(guild.getID())) {
 			CompletableFuture<VoiceConnection> future = RBMusic.joinVoice(member, (IGuildTextChannel) message.getChannel());
 			future.thenAcceptAsync(vc -> {
